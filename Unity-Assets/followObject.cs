c604cc4ca541bf3a50323eda2265a92d3b322c09
Calos Chacin <Carlos@Caloss-MacBook-Pro.local>
2017-09-06 14:45:25 -0400

    Improved game example demo movement and camera for gyroscope

---
+++ Unity-Assets/followObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followObject : MonoBehaviour {
+	//public GameObject target;
+	public Transform target;
+    public float smoothTime = 0.5F;
+    private Vector3 velocity = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, 0));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);	
+	}
+}
--- Unity-Assets/stressTest.cs
+++ Unity-Assets/stressTest.cs
@@ -14,29 +14,8 @@ public class stressTest : MonoBehaviour{
     void Update () {
 
         pitch = Mathf.Floor(MATRIX.Gyroscope.pitch)*0.5f;
-        //y = GameController.Gyroscope.y * Time.deltaTime * 2;
-
         roll = Mathf.Floor(MATRIX.Gyroscope.roll)*0.5f;
-        //x = GameController.Gyroscope.x * Time.deltaTime * 2;
-
-
+        
         transform.position = new Vector3(pitch, 2, roll);
-
-
-
-
-        //Adjusted Values
-        //transform.Rotate(GameController.pitch, 0, 0, Space.World);
-        //Quaternion AddRot = Quaternion.identity;
-        //pitch = GameController.roll * (Time.deltaTime * 2);
-        //roll = GameController.pitch * (Time.deltaTime * 2) * -1;
-
-        //AddRot.eulerAngles = new Vector3(pitch, yaw, roll);
-        //transform.rotation *= AddRot;
-        //Vector3 AddPos = Vector3.forward;
-        //AddPos = transform.rotation * AddPos;
-        //GetComponent<Rigidbody>().velocity = AddPos * (Time.deltaTime * 20.0f);//orig 2000
-
-        //AddRot.eulerAngles = new Vector3(pitch, yaw, roll);
     }
 }
